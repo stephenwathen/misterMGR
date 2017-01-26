4ba087c870ea451200f7c691632f208af35f18e5
stephenwathen <stephen.p.wathen@gmail.com>
2017-01-25 18:19:36 -0700

    misterMGR_1

---
+++ index.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+ 	//modal functionality: pull the div/div id, on click open modal
+ 	$(.grid_item #1).click(function){
+ 		$(event).preventDefault()
+ 		$(.modal #1).fadetoggle()
+ 	});
+ 	$(.grid_item #2).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #3).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #4).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #5).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #6).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #7).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #8).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #9).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #10).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #11).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	$(.grid_item #12).click(){
+ 		$(.modal).fadeIn();
+ 	}
+ 	
+});
